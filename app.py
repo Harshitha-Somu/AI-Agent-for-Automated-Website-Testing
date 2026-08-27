6dba0f49ef7dc92939db8a8b5cf807d273980cbd
Harshitha Somu <harshithasomu60@gmail.com>
2026-08-27 22:35:23 +0530

    Fix Docker Playwright deployment

--- app.py
+++ app.py
@@ -149,4 +149,6 @@ os.makedirs("static/screenshots", exist_ok=True)
 os.makedirs("static/reports", exist_ok=True)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    #app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
